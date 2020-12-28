63dddfb2ea4a7b47519b46d0d8ccea0b4621dcfb
Ruhulcse <ruhul.cse7862@gmail.com>
2020-12-28 15:52:48 +0600

    GET request [get all post]

--- Routes/Users.js
+++ Routes/Users.js
@@ -4,23 +4,29 @@ const router = express.Router();
 const User = require('../Models/Users');
 
 
-router.get('/',(req,res) => {
-    res.send("From User screen section");
+router.get('/',async(req,res) => {
+    try {
+        const users = await User.find();
+        res.json(users)
+    } catch (error) {
+        res.json({message : error});
+    }
 });
 
-router.post('/',(req,res) =>{
+router.post('/',async(req,res) =>{
+    console.log(req.body);
      const user = new User({
          name : req.body.name,
          email: req.body.email,
          password: req.body.password,
          role: req.body.role
      });
-     user.save()
-     .then( data => {
-         res.json(data);
-     })
-     .catch(err => {
-         res.json({ message : err});
-     })
+    try {
+        const SavedUser = await user.save();
+        res.json(SavedUser);
+    } catch (error) {
+        res.json({message: error});
+
+    }
 });
 module.exports = router;
