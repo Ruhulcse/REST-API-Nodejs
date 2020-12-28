f481c21ad782587a0166138ee1eaee2bbc1f7234
Ruhulcse <ruhul.cse7862@gmail.com>
2020-12-28 12:49:59 +0600

    database connect

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+//ROUTES
 app.get('/',(req,res) => {
     res.send("home secreen");
 })
 
+
+//Connect To DB
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true ,
+      useUnifiedTopology: true
+    },
+    ()=>console.log("Connected to DB")
+);
+//Listening server
 app.listen(port, ()=> {
     console.log("app is listening");
 })
